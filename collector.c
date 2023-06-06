220e83f28da26bda579bfaa2d33e5254ee9e4115
DevHgL <hleomelo1@icloud.com>
2023-06-06 15:01:17 -0300

    commit

--- collector.c
+++ collector.c
@@ -1,4 +1,6 @@
-#include "libgarbage.h"
+#include "collector.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_OBJECTS 100
 
@@ -16,19 +18,23 @@ void* malloc2(size_t size) {
 void coleta() {
     for (int i = 0; i < num_objects; i++) {
         free(objects[i]);
-        objects[i] = NULL;
     }
     num_objects = 0;
 }
 
-void atrib2(void** ptr, void* value) {
-    coleta();
-    *ptr = value;
+void atrib2(void** ptr, void* new_ptr) {
+    for (int i = 0; i < num_objects; i++) {
+        if (objects[i] == *ptr) {
+            objects[i] = new_ptr;
+            break;
+        }
+    }
+    *ptr = new_ptr;
 }
 
 void dump() {
-    printf("Dumping objects:\n");
+    printf("Objetos alocados: %d\n", num_objects);
     for (int i = 0; i < num_objects; i++) {
-        printf("Object %d: %p\n", i, objects[i]);
+        printf("Endereco: %p\n", objects[i]);
     }
 }
--- main.c
+++ main.c
@@ -1,18 +1,31 @@
+#include "collector.h"
 #include <stdio.h>
-#include "libgarbage.h"
 
 int main() {
+    printf("Antes da alocacao:\n");
+    dump();
+
     int* ptr1 = malloc2(sizeof(int));
-    int* ptr2 = malloc2(sizeof(int));
-    
-    atrib2((void**)&ptr1, NULL);
-    atrib2((void**)&ptr2, NULL);
-    
+    *ptr1 = 10;
+
+    char* ptr2 = malloc2(sizeof(char));
+    *ptr2 = 'a';
+
+    printf("Apos alocacao:\n");
     dump();
-    
+
+    int* ptr3 = malloc2(sizeof(int));
+    *ptr3 = 20;
+
+    atrib2((void**)&ptr1, ptr3);
+
+    printf("Apos atribuicao.\n");
+    dump();
+
     coleta();
-    
+
+    printf("Apos coleta.\n");
     dump();
-    
+
     return 0;
 }
